222d737fcf516d8c8e3321fd2cfed4b76d584754
gowtham758550 <gowtham758550@gmail.com>
2022-07-30 12:34:03 +0530

    dashboard created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown'
+
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchBarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    InputTextModule,
+    CardModule,
+    ButtonModule,
+    DropdownModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    SearchBarComponent
   ]
 })
 export class SharedModule { }
