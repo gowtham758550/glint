3bbc5c472148329902beaa53a4b5b00fa7b59698
deebiKaaRaviSankar <deepiravi300@gmail.com>
2022-07-31 11:00:26 +0530

    Added routing paths to login and register

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { UserLoginComponent } from "./components/user-login/user-login.component";
+import { RegisterComponent } from "./components/User-register/register.component";
 
 const routes: Routes = [
-    { path: '', component: LandingPageComponent }
+    { path: '', component: LandingPageComponent },
+    {path:'register' , component: RegisterComponent},
+    {path:'login' , component: UserLoginComponent}
 ];
 
 @NgModule({
