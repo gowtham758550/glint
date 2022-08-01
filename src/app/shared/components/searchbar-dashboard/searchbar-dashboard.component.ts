eac173345f59b369051b0f6b8fb280d93a8b04b2
deebiKaaRaviSankar <deepiravi300@gmail.com>
2022-08-01 14:45:36 +0530

    Added search bar for dashboard component

---
+++ src/app/shared/components/searchbar-dashboard/searchbar-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar-dashboard',
+  templateUrl: 'searchbar-dashboard.component.html',
+  styleUrls: ['searchbar-dashboard.component.css']
+})
+export class SearchbarDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
