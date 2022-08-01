424ef20c41c1c47cc916d4e84fd69548625e6ed3
deebiKaaRaviSankar <deepiravi300@gmail.com>
2022-08-01 14:44:57 +0530

    Added dashboard component. In progress

--- src/app/features/job-seeker/dashboard/dashboard.component.ts
+++ src/app/features/job-seeker/dashboard/dashboard.component.ts
@@ -2,15 +2,53 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: `
-    <p>
-      dashboard works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  Jobs: any = [{
+    image: '/assets/Conzio.jpg',
+    Company_Name: 'Conzio Constructions',
+    Job_title: 'Software Engineer',
+    Location: 'Coimbatore',
+    CTC: 25000,
+    Job_Type: 'Full Time',
+
+  }, {
+    image: '/assets/Midway Overline.jpg',
+    Company_Name: 'Midway Overline',
+    Job_title: 'Software Engineer',
+    Location: 'Coimbatore',
+    CTC: 50000,
+    Job_Type: 'Full Time'
+  },
+  {
+    image: '/assets/Midway Overline.jpg',
+    Company_Name: 'Midway Overline',
+    Job_title: 'Software Engineer',
+    Location: 'Coimbatore',
+    CTC: 50000,
+    Job_Type: 'Full Time'
+  },
+  {
+    image: '/assets/Midway Overline.jpg',
+    Company_Name: 'Midway Overline',
+    Job_title: 'Software Engineer',
+    Location: 'Coimbatore',
+    CTC: 50000,
+    Job_Type: 'Full Time'
+  },
+  {
+    image: '/assets/Midway Overline.jpg',
+    Company_Name: 'Midway Overline',
+    Job_title: 'Software Engineer',
+    Location: 'Coimbatore',
+    CTC: 50000,
+    Job_Type: 'Full Time'
+  }];
+  
+  
+
 
   constructor() { }
 
