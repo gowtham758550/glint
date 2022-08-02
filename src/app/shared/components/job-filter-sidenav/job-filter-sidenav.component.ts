4cbb2298830431ddc9032505fb0b31657486122b
deebiKaaRaviSankar <deepiravi300@gmail.com>
2022-08-02 09:23:57 +0530

    Emitted the filtered options to dashboard

--- src/app/shared/components/job-filter-sidenav/job-filter-sidenav.component.ts
+++ src/app/shared/components/job-filter-sidenav/job-filter-sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { filter } from 'rxjs';
 
 @Component({
@@ -9,32 +9,33 @@ import { filter } from 'rxjs';
 export class JobFilterSidenavComponent implements OnInit {
 
   selectedCategories: any[] = [];
-  selectedDesignation:any[]=[];
-  selectedLocation:any[]=[];
-  CategoryExperienceBased: any[] = [{name: 'Experienced', key: 'E'}, {name: 'Fresher', key: 'F'}];
+  selectedDesignation: any[] = [];
+  selectedLocation: any[] = [];
+  CategoryExperienceBased: any[] = [{ name: 'Experienced', key: 'E' }, { name: 'Fresher', key: 'F' }];
   Designation: any[] = [
-  {title: 'Full Stack Developer', key: 'FSD'}, 
-  {title: 'Backend Developer', key: 'BD'},
-  {title: 'Frontend Developer', key: 'FD'},
-  {title: 'Quality Analyst', key: 'QA'},
-  {title: 'Android Developer', key: 'AD'},
-  {title: 'IOS Developer', key: 'IOS',}
-];
-Location: any[] = [{location: 'Chennai', key: 'C'}, {location: 'Bangalore', key: 'B'},{location:'Hyderabad',key:'H'},{location:'Coimbatore', key:'Co'}];
+    { title: 'Full Stack Developer', key: 'FSD' },
+    { title: 'Backend Developer', key: 'BD' },
+    { title: 'Frontend Developer', key: 'FD' },
+    { title: 'Quality Analyst', key: 'QA' },
+    { title: 'Android Developer', key: 'AD' },
+    { title: 'IOS Developer', key: 'IOS', }
+  ];
+  Location: any[] = [{ location: 'Chennai', key: 'C' }, { location: 'Bangalore', key: 'B' }, { location: 'Hyderabad', key: 'H' }, { location: 'Coimbatore', key: 'Co' }];
 
+  @Output() getSelectedExperience = new EventEmitter<any>();
+  getFilteredExperience() {
+    this.getSelectedExperience.emit(this.selectedCategories);
+  }
+  @Output() getSelectedDesignation = new EventEmitter<any>();
+  getFilteredDesignation(){
+    this.getSelectedDesignation.emit(this.selectedDesignation);
+  }
   constructor() { }
 
   ngOnInit(): void {
-  this.selectedDesignation=this.Designation.slice(6,7);
-  this.selectedCategories = this.CategoryExperienceBased.slice(2,3);
-  this.selectedLocation=this.Location.slice(4,4);
-  // console.log(this.selectedCategories);
-  }
-  print(){
-  //   this.selectedDesignation=this.Designation;
-  // this.selectedCategories = this.CategoryExperienceBased;
-    console.log(this.selectedDesignation);
-    console.log(this.selectedCategories);
+    this.selectedDesignation = this.Designation.slice(6, 7);
+    this.selectedCategories = this.CategoryExperienceBased.slice(2, 3);
+    this.selectedLocation = this.Location.slice(4, 4);
   }
 
 }
